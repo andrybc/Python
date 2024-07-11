f4ea49c40bd54ee0ae55f8b67a7fd6dc643f3a74
andrybc <canelbandry@gmail.com>
2024-07-10 23:57:12 -0400

    learning tkinder and creating buttons and labels. Retrieving an api key from open weather api and going to implement the request function

--- IntermediateProj/weather_app.py
+++ IntermediateProj/weather_app.py
@@ -6,8 +6,23 @@ from tkinter import messagebox
 #create the main window
 root = tk.Tk()
 root.title("Weather App")
+root.geometry("600x600")
+
+city_label = tk.Label(root, text="City: ")
+city_label.pack(pady=10)
+city_entry = tk.Entry(root)
+city_entry.pack()
+
+get_button = tk.Button(root, text="Retrieve Weather", command=get_weather)
+get_button.pack()
+
+weather_label = tk.Label(root, text="")
+weather_label.pack()
 
 
+
+def get_weather():
+    pass
 
 
 root.mainloop()
