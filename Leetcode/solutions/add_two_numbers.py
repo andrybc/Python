82fa411ef6feece7ee46cded0a19fb8c585bdd99
andrybc <canelbandry@gmail.com>
2024-11-09 04:02:42 -0500

    cleaned up the redundancy in add_two_numbers and learned about conditional expressions (ternary operators) to calculated a conditional value for current_node.val. Learned that objects can be checked if they are NULL by just the object name

--- Leetcode/solutions/add_two_numbers.py
+++ Leetcode/solutions/add_two_numbers.py
@@ -13,38 +13,59 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        #previous accepted code
         #print(l1)
         #print(l2)
-        ansHead = ListNode()
-        current = ansHead
-        current_Carry = 0
-        while l1 != None or l2 != None or current_Carry==1:
-            #curr = current
-            if l1==None and l2==None and current_Carry==1:
-                current.val = current_Carry
+        # ansHead = ListNode()
+        # current = ansHead
+        # current_Carry = 0
+        # while l1 != None or l2 != None or current_Carry==1:
+        #     curr = current
+        #     if l1==None and l2==None and current_Carry==1:
+        #         curr.val = current_Carry
                 
-            elif l1 == None:
-                current.val = l2.val + current_Carry
-                l2 = l2.next
-            elif l2 == None:
-                current.val = l1.val + current_Carry
-                l1 = l1.next
+        #     elif l1 == None:
+        #         curr.val = l2.val + current_Carry
+        #         l2 = l2.next
+        #     elif l2 == None:
+        #         curr.val = l1.val + current_Carry
+        #         l1 = l1.next
 
-            else:
-                current.val = l1.val + l2.val + current_Carry
-                l1 = l1.next
-                l2 = l2.next
+        #     else:
+        #         curr.val = l1.val + l2.val + current_Carry
+        #         l1 = l1.next
+        #         l2 = l2.next
                 
                 
-            if current.val >=10:
-                current.val -=10
-                current_Carry =1
-            else:
-                current_Carry = 0
+        #     if curr.val >=10:
+        #         curr.val -=10
+        #         current_Carry =1
+        #     else:
+        #         current_Carry = 0
             
-            if l1 != None or l2!= None or current_Carry ==1:
-                current.next = ListNode()
-                current = current.next
+        #     if l1 != None or l2!= None or current_Carry ==1:
+        #         current.next = ListNode()
+        #         current = current.
+                
+        #cleaned up code      
+        sumHead = ListNode()
+        current_node = sumHead
+        carry = 0
+        while  l1  or  l2  or carry:
+            current_node.val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry = current_node.val //10
+            current_node.val %= 10
+            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            
+            if l1 or l2 or carry:
+                current_node.next = ListNode()
+                current_node = current_node.next
               
-        return ansHead  
+        return sumHead  
+              
+    
          
